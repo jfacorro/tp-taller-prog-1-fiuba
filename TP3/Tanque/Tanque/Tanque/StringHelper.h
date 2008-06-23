cb4aa4579bbe8e6143cc20984854e68edf60afd0
juan.facorro <juan.facorro@6dff32cf-8f48-0410-9a2e-7b8b34aa6dfb>
2008-06-23 03:31:26 +0000

    Agregado de soporte para texturas.

---
+++ TP3/Tanque/Tanque/Tanque/StringHelper.h
@@ -0,0 +1,19 @@
+#include <string.h>
+
+#ifndef StringHelper_h
+
+#define StringHelper_h
+
+class StringHelper
+{
+	public:
+		static int FindFirstOcurrence(char * line, int len, int offset, char character);
+		static char * Substring(char * str, int begin, int length);
+		static char * Trim(char * str);
+		static char * GetEmptyString();
+		static char * AppendString(char * str1, char * str2);
+        static void Copy(char * src, char * dst, int length);
+        
+};
+
+#endif
